1bc4b18fe972eaa82aee87cfb1d041464545aed2
Treesmurf <troyanderson1988@gmail.com>
2026-04-22 08:18:27 +1000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA8aiXqcGdq8E-KL7fdj_1TVNasLeFRz-o",
@@ -12,3 +13,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+// Keep Troy signed in across page refreshes
+setPersistence(auth, browserLocalPersistence);
